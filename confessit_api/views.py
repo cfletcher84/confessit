97127d1219cf22d61ca508d94aa97f3386e14721
Fletch <fletcher.d3v@gmail.com>
2024-09-17 12:21:12 -0500

    Added required authentication for feed

--- confessit_api/views.py
+++ confessit_api/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import status, viewsets, filters
 from confessit_api import serializers, models, permissions
 
@@ -107,5 +108,15 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 
 class UserLoginApiView(ObtainAuthToken):
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
-    
-    
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+    permission_classes = (
+        permissions.UpdateOwnStatus,
+        IsAuthenticated
+    )
+
+    def perform_create(self, serializer):
+        serializer.save(user_profile=self.request.user)
